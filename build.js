110d95c0385fea31dc601be45d4477e05cbf5010
Cat Core <arthur.koalas@free.fr>
2020-05-01 17:13:58 +0200

    Oups

--- build.js
+++ build.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 var text = `GOOGLE_SERVICE_ACCOUNT_EMAIL=${process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL}\GOOGLE_PRIVATE_KEY=${process.env.GOOGLE_PRIVATE_KEY}\n`
-fs.writeFileSync('./.env', )
+fs.writeFileSync('./.env', text)
