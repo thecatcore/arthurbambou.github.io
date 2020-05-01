d40474fa3f4073cafe02edf4898f2c0f815298ed
Cat Core <arthur.koalas@free.fr>
2020-05-01 17:17:48 +0200

    Fix?

--- build.js
+++ build.js
@@ -1,3 +1,4 @@
 const fs = require('fs')
-var text = `GOOGLE_SERVICE_ACCOUNT_EMAIL=${process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL}\GOOGLE_PRIVATE_KEY=${process.env.GOOGLE_PRIVATE_KEY}\n`
+var text = `GOOGLE_SERVICE_ACCOUNT_EMAIL=${process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL}\nGOOGLE_PRIVATE_KEY=${process.env.GOOGLE_PRIVATE_KEY}\n`
+console.log(text)
 fs.writeFileSync('./.env', text)
